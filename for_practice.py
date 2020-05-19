6d5bd644abff20b6aafd16f162aea4bcfb693361
eun byeol kang <eunbyoul0421@naver.com>
2020-05-19 15:58:32 +0900

    N자리수의 각 자리 숫자합 출력하기

--- for_practice.py
+++ for_practice.py
@@ -4,7 +4,25 @@
 #     print(7, "x", i, "=", 7*i)
 
 #구구단 2~9단까지 출력하기
-for dan in range(2,10):
-    print("-"*10)
-    for i in range(1,10):
-        print(dan, "x", i, "=", dan*i)
+# for dan in range(2,10):
+#     print("-"*10)
+#     for i in range(1,10):
+#         print(dan, "x", i, "=", dan*i)
+
+#N자리수의 각 자리 숫자합 출력하기
+num = input("숫자를 입력하세요 : ")
+sum = 0
+#0~입력받은 숫자 길이만큼 하나씩 꺼냄 -> n
+#i: 입력받은 숫자 길이 -1
+for i in range(0, len(num)):
+    n = num[i]
+    sum += int(n)
+
+#num에 더하자
+#sum 출력하자
+print(num)
+
+#369 게임
+#전화번호부 만들고, 이름으로 검색하기
+#주차요금 계산하기, 최초 30분 2800원, 10분당 1000원, 2500원 넘을 수 없음
+#야구게임
