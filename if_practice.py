fd75b014a852414f06897772f1b3156a2558b710
eun byeol kang <eunbyoul0421@naver.com>
2020-05-19 14:48:10 +0900

    학번 -> 학년, 학과, 반, 번호 분석하기

--- if_practice.py
+++ if_practice.py
@@ -16,12 +16,27 @@
 
 # 평점 매기기. 90 이상 : A, 80 이상 90 미만 : B, 70 이상 80 미만 : C, 70 미만 : F
 
-score = int(input("당신의 점수는?"))
-if 90 <= score:
-    print("A")
-elif 80 <= score < 90:
-    print("B")
-elif 70 <= score < 80:
-    print("C")
-elif score < 70:
-    print("F")
+# score = int(input("당신의 점수는?"))
+# if 90 <= score:
+#     print("A")
+# elif 80 <= score < 90:
+#     print("B")
+# elif 70 <= score < 80:
+#     print("C")
+# elif score < 70:
+#     print("F")
+
+# 학번 -> 학년, 학과, 반, 번호 분석하기
+student_number = input("당신의 학번은: ")
+grade = student_number[0]
+classroom = student_number[1]
+major = "null"
+if classroom == "1" or classroom == "2":
+    major = "뉴미디어소프트웨어"
+elif classroom == "3" or classroom == "4":
+    major = "뉴미디어웹솔루션"
+elif classroom == "5" or classroom == "5":
+    major = "뉴미디어디자인"
+
+number = student_number[2:]
+print(grade+"학년 "+ major+ "과 "+ classroom+ "반 "+ number+ "번입니다.")
