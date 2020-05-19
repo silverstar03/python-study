4386f3e0e947c7162b42b0e23043bba33129243a
eun byeol kang <eunbyoul0421@naver.com>
2020-05-19 15:13:21 +0900

    구구단 2~9단 출력하기

--- for_practice.py
+++ for_practice.py
@@ -1,4 +1,9 @@
 # for 연습
 # 구구단 7단 출력하기
-for i in range(1, 10): #주어진 숫자가 눈에 보이게 함(10을 9+1로)
-    print(7, "x", i, "=", 7*i)
+# for i in range(1, 10): #주어진 숫자가 눈에 보이게 함(10을 9+1로)
+#     print(7, "x", i, "=", 7*i)
+
+#구구단 2~9단까지 출력하기
+for dan in range(2,10):
+    for i in range(1,10):
+        print(dan, "x", i, "=", dan*i)
